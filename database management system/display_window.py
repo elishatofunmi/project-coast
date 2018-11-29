512e211a8be3736af186cbd6e7ff18080290c4b3
Odemakinde Elisha Jesutofunmi <elishatofunmi@gmail.com>
2018-11-29 20:09:29 +0000

    Add files via upload

--- database management system/display_window.py
+++ database management system/display_window.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import numpy as np
-
+from subprocess import PIPE,Popen
 from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout, QLabel, QVBoxLayout, QLineEdit, QStackedLayout
 from database import Database
 
@@ -334,7 +334,8 @@ class edit_officer(QWidget):
        self.edit_name = QLineEdit('', self)
        self.edit_name.move(100,30)
        self.edit_name.resize(400, 30)
-       self.edit_name.setPlaceholderText("Enter officer name")
+       #self.edit_name.setPlaceholderText("Enter officer name")
+       self.edit_name.seText(str(self.attr[0]))
        layout.addWidget(self.edit_name,1,4,1,5)
        
 
@@ -361,7 +362,8 @@ class edit_officer(QWidget):
        self.rank_box.move(100,70)
        self.rank_box.resize(400,30)
        layout.addWidget(self.rank_box,3,4,1,5)
-       self.rank_box.setPlaceholderText("Enter your rank")
+       #self.rank_box.setPlaceholderText("Enter your rank")
+       self.rank_box.setText(str(self.attr[1]))
        
        
 
@@ -377,7 +379,8 @@ class edit_officer(QWidget):
        self.enter_unit.move(100,110)
        self.enter_unit.resize(400,30)
        layout.addWidget(self.enter_unit,4,4,1,5)
-       self.enter_unit.setPlaceholderText("Enter Unit")
+       #self.enter_unit.setPlaceholderText("Enter Unit")
+       self.enter_unit.setText(str(self.attr[2]))
 
 
 
@@ -940,7 +943,7 @@ class edit_soldier(QWidget):
       
        try:
           destination = new_path
-          os.replace(source_file, new_path)
+          os.remove(filePath)
        except FileNotFoundError:
           self.get_image.save(new_path)
           
